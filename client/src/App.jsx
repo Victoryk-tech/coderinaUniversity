681d7335eab10a2d3dcc62d7c3443fce82e4dbf9
VictoryK-tech <kemelevictory3802@gmail.com>
2024-09-26 19:02:19 +0100

    first

---
+++ client/src/App.jsx
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+
+import "./App.css";
+import RegistrationPage from "./pages/RegistrationPage";
+import Loader from "./pages/LoadingScreen";
+
+function App() {
+  const [isLoading, setIsLoading] = useState(true); // Default to false, so the loader starts
+
+  return (
+    <div>
+      {isLoading && <Loader setIsLoading={setIsLoading} />}{" "}
+      {/* Show Loader if loading */}
+      {!isLoading && <RegistrationPage />}{" "}
+      {/* Show RegistrationPage after loading */}
+    </div>
+  );
+}
+
+export default App;
