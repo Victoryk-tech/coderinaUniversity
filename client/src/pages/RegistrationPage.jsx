263e8c66ccaf1fe1337ff5564b5676e483255117
VictoryK-tech <kemelevictory3802@gmail.com>
2024-09-28 00:11:52 +0100

    added

--- client/src/pages/RegistrationPage.jsx
+++ client/src/pages/RegistrationPage.jsx
@@ -8,7 +8,7 @@ const RegistrationPage = () => {
       </div>
       <div className=" lg:px-20 py-4">
         {" "}
-        <div className="flex flex-col items-start justify-start px-2 md:px-16 md:py-16 w-full shadow-md bg-white">
+        <div className="flex flex-col items-start justify-start px-2 md:px-16 py-6 md:py-16 w-full shadow-md bg-white">
           <div className="w-full pt-5 md:pt-1 text-2xl lg:text-3xl font-bold md:font-semibold pb-6 lg:pb-8 border-b-[1px] border-slate-300">
             <h1 className="text-[#39f]">UNIVERSITY CHALLENGE REGISTRATION</h1>
           </div>
@@ -33,7 +33,7 @@ const RegistrationPage = () => {
                       <label htmlFor="">First name*</label>
                       <input
                         type="text"
-                        className=" pl-2 border-[1px] border-slate-300 rounded-md w-full md:w-80 py-1"
+                        className="w-full pl-2 border-[1px] border-slate-300 rounded-md md:w-80 py-1"
                       />
                     </div>
                     <div className="flex flex-col items-start justify-start space-y-1">
@@ -41,7 +41,7 @@ const RegistrationPage = () => {
                       <input
                         type="Email"
                         placeholder="only university/college emails accepted"
-                        className=" pl-2 border-[1px] border-slate-300 rounded-md md:w-80 py-1 placeholder:text-sm placeholder:pl-4"
+                        className=" pl-2 border-[1px] border-slate-300 rounded-md  md:w-80 py-1 placeholder:text-sm placeholder:pl-4"
                       />
                     </div>
                     <div className="flex flex-col items-start justify-start space-y-1">
@@ -73,7 +73,7 @@ const RegistrationPage = () => {
                 </div>
               </div>
 
-              <div className="lg:py-4 space-y-6">
+              <div className="py-4 space-y-6">
                 <h2 className="text-xl font-medium">
                   Team memeber 2 (optional)
                 </h2>
@@ -109,7 +109,7 @@ const RegistrationPage = () => {
                 </div>
               </div>
 
-              <div className="lg:py-10 space-y-6">
+              <div className="py-6 lg:py-10 space-y-6">
                 <h2 className="text-xl font-medium">
                   Team memeber 3 (optional)
                 </h2>
